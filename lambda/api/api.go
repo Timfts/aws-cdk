8ff72018b003a0b5cab9d8b1f8d688ce8f33678d
Tim Fontes <italo5123@hotmail.com>
2024-07-21 20:27:00 -0300

    feat: :sparkles: added dynamo db to project

---
+++ lambda/api/api.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"fmt"
+	"lambda-func/database"
+	"lambda-func/types"
+)
+
+type ApiHandler struct {
+	dbStore database.DynamoDBClient
+}
+
+func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
+	return ApiHandler{
+		dbStore: dbStore,
+	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("request has empty params")
+	}
+
+	userExists, err := api.dbStore.DoesUserExist(event.Username)
+	if err != nil {
+		return fmt.Errorf("something unexpected happened, please try again later")
+	}
+
+	if userExists {
+		return fmt.Errorf("User already exists")
+	}
+
+	err = api.dbStore.InsertUser(event)
+	if err != nil {
+		return fmt.Errorf("error registering user, try again later")
+	}
+
+	return nil
+}
--- lambda/go.mod
+++ lambda/go.mod
@@ -2,4 +2,8 @@ module lambda-func
 
 go 1.19
 
-require github.com/aws/aws-lambda-go v1.47.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	github.com/aws/aws-sdk-go v1.54.20 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
